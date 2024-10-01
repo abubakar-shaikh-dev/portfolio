e27332d8deea467598a9c7da27582667a64d78ae
Abubakar Shaikh (XERY) <shaikhabubakar2380@gmail.com>
2024-10-01 06:45:48 +0530

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Abubakar Shaikh | Software Development Engineer",
+  description: "Explore the portfolio of Abubakar Shaikh, a skilled Software Development Engineer specializing in building robust and scalable web applications. Discover projects, skills, and professional experiences.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
